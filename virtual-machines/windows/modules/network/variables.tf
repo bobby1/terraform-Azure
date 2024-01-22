ec9cd7709f222ed2971bd62eff693a1043430840
Bobby1 <bwen@yahoo.com>
2024-01-22 14:39:40 -0800

    Test of deployment size

--- virtual-machines/windows/modules/network/variables.tf
+++ virtual-machines/windows/modules/network/variables.tf
@@ -1,8 +1,24 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
+###======================================================================================
+### Copyright (c) 2024, Bobby Wen, All Rights Reserved 
+### Use of this source code is governed by a MIT-style
+### license that can be found at https://en.wikipedia.org/wiki/MIT_License.
+### Project:		Microsoft Azurerm examples 
+### Class:			Terraform Azurerm IaC file
+### Purpose:    Terraform script to create Microsoft Azure Windows server with Active directory controller
+### Usage:			terraform (init|plan|apply|destroy)
+### Pre-requisites:	Azure subscription (https://azure.microsoft.com/en-us/), 
+###                 Terraform by HashiCorp (https://www.terraform.io/)
+### Beware:     Variables.tf file is used to pass environment variable to main.tf.  
+###             Depending on SDLC environmental setting, different attributes are passed to create the stack 
+###
+### Developer: 	Bobby Wen, bobby@wen.org
+###======================================================================================
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+}
+
+variable "environment" {
+  description = "SDLC Infrastructure environment: THIS SETS THE DEPLOYMENT ENVIRONMENT"
 }
 
 variable "location" {
