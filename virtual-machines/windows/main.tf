b249538e4798d598f01bfeae8bca63e1b5f82f2a
Bobby1 <bwen@yahoo.com>
2024-01-22 14:38:50 -0800

    Test of deployment size

--- virtual-machines/windows/main.tf
+++ virtual-machines/windows/main.tf
@@ -1,44 +1,57 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
+###======================================================================================
+### Copyright (c) 2024, Bobby Wen, All Rights Reserved 
+### Use of this source code is governed by a MIT-style
+### license that can be found at https://en.wikipedia.org/wiki/MIT_License.
+### Project:		Microsoft Azurerm examples 
+### Class:			Terraform Azurerm IaC file
+### Purpose:    Terraform script to create Microsoft Azure Windows server with Active directory controller
+### Usage:			terraform (init|plan|apply|destroy)
+### Pre-requisites:	Azure subscription (https://azure.microsoft.com/en-us/), 
+###                 Terraform by HashiCorp (https://www.terraform.io/)
+### Beware:     Variables.tf file is used to pass environment variable to main.tf.  
+###             Depending on SDLC environmental setting, different attributes are passed to create the stack 
+###
+### Developer: 	Bobby Wen, bobby@wen.org
+###======================================================================================
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
-  location = var.location
+  location = var.location[var.environment]
   name     = "${var.prefix}-rg"
+  tags     = local.tags
 }
 
 module "network" {
-  source = "./modules/network"
-
-  location            = var.location
-  prefix              = var.prefix
+  source              = "./modules/network"
+  environment         = var.environment
+  location            = var.location[var.environment]
   resource_group_name = azurerm_resource_group.example.name
+  prefix              = var.prefix
 }
 
 module "active-directory-domain" {
-  source = "./modules/active-directory-domain"
-
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-
+  source                        = "./modules/active-directory-domain"
+  environment                   = var.environment
+  location                      = azurerm_resource_group.example.location
+  resource_group_name           = azurerm_resource_group.example.name
   active_directory_domain_name  = "${var.prefix}.local"
   active_directory_netbios_name = var.prefix
   admin_username                = var.admin_username
   admin_password                = var.admin_password
   prefix                        = var.prefix
   subnet_id                     = module.network.domain_controllers_subnet_id
+  ### NOTE: AD requires a larger VM size,  It is hard coded to Standard_F2  in modules/active-directory-domain/main.tf
 }
 
 module "active-directory-member" {
-  source = "./modules/domain-member"
-
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  prefix              = var.prefix
-
+  source                       = "./modules/domain-member"
+  environment                  = var.environment
+  location                     = azurerm_resource_group.example.location
+  resource_group_name          = azurerm_resource_group.example.name
+  size                         = var.az_instance_type[var.environment]
+  prefix                       = var.prefix
   active_directory_domain_name = "${var.prefix}.local"
   active_directory_username    = var.admin_username
   active_directory_password    = var.admin_password
--- virtual-machines/windows/variables.tf
+++ virtual-machines/windows/variables.tf
@@ -1,16 +1,60 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+###======================================================================================
+### Copyright (c) 2024, Bobby Wen, All Rights Reserved 
+### Use of this source code is governed by a MIT-style
+### license that can be found at https://en.wikipedia.org/wiki/MIT_License.
+### Project:		Microsoft Azurerm examples 
+### Class:			Terraform Azurerm IaC file
+### Purpose:    Terraform script to create Microsoft Azure Windows server with Active directory controller
+### Usage:			terraform (init|plan|apply|destroy)
+### Pre-requisites:	Azure subscription (https://azure.microsoft.com/en-us/), 
+###                 Terraform by HashiCorp (https://www.terraform.io/)
+### Beware:     Variables.tf file is used to pass environment variable to main.tf.  
+###             Depending on SDLC environmental setting, different attributes are passed to create the stack 
+###
+### Developer: 	Bobby Wen, bobby@wen.org
+###======================================================================================
+variable "project_name" {
+  description = "product or project name"
+  type        = string
+  default     = "DevOps tools team"
+}
 
 variable "prefix" {
-  description = "The prefix which should be used for all resources in this example"
+  description = "A prefix used for all resources in this example"
   type        = string
-  default     = "b1AZtest"
+  default     = "b1AZwnd"
+}
+
+variable "environment" {
+  description = "SDLC Infrastructure environment: THIS SETS THE DEPLOYMENT ENVIRONMENT"
+  type        = string
+  default     = "dev"
+}
+
+variable "instance_name" {
+  description = "Value of the Name tag for the instance"
+  type        = string
+  default     = "DevOps_team"
 }
 
 variable "location" {
-  description = "The Azure Region in which all resources in this example should be created."
-  type        = string
-  default     = "westus"
+  description = "The Azure Region in which all resources in this environment should be provisioned"
+  type        = map(string)
+  default = {
+    dev = "westus"
+    stg = "westus2"
+    prd = "westus3"
+  }
+}
+
+variable "az_instance_type" {
+  description = "Azure instance type"
+  type        = map(string)
+  default = {
+    dev = "Standard_B1s"
+    stg = "Standard_F2"
+    prd = "Standard_DS2_v2"
+  }
 }
 
 variable "admin_username" {
@@ -25,3 +69,10 @@ variable "admin_password" {
   default     = "AZPassword230118!"
 }
 
+locals {
+  tags = {
+    project     = var.project_name
+    environment = var.environment
+    name        = var.instance_name
+  }
+}
