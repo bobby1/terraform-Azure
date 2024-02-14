7bcd10f3d90cd998681ee980608088816ca859d4
Bobby1 <bwen@yahoo.com>
2024-02-14 09:52:57 -0800

    Updated for note on having enough quote for VM type

--- kubernetes-cluster/main.tf
+++ kubernetes-cluster/main.tf
@@ -1,24 +1,24 @@
-###======================================================================================
-### Copyright (c) 2024, Bobby Wen, All Rights Reserved 
-### Use of this source code is governed by a MIT-style
-### license that can be found at https://en.wikipedia.org/wiki/MIT_License.
-### Project:		Microsoft Azurerm Kubernetes examples 
-### Class:			Terraform Azurerm IaC file
-### Purpose:    Terraform script to create Microsoft Azure Kubernetes Cluster with Monitoring and public IP
-### Usage:			terraform (init|plan|apply|destroy)
-### Pre-requisites:	Azure subscription (https://azure.microsoft.com/en-us/), 
-###                 Terraform by HashiCorp (https://www.terraform.io/)
-### Beware:     Variables.tf file is used to pass environment variable to main.tf.  
-###             Depending on SDLC environmental setting, different attributes are passed to create the stack 
-###
-### Developer: 	Bobby Wen, bobby@wen.org
-###======================================================================================
+//======================================================================================
+// Copyright (c) 2024, Bobby Wen, All Rights Reserved 
+// Use of this source code is governed by a MIT-style
+// license that can be found at https://en.wikipedia.org/wiki/MIT_License.
+// Project:		Microsoft Azurerm Kubernetes examples 
+// Class:			Terraform Azurerm IaC file
+// Purpose:    Terraform script to create Microsoft Azure Kubernetes Cluster with Monitoring and public IP
+// Usage:			terraform (init|plan|apply|destroy)
+// Pre-requisites:	Azure subscription (https://azure.microsoft.com/en-us/), 
+//                 Terraform by HashiCorp (https://www.terraform.io/)
+// Beware:     Variables.tf file is used to pass environment variable to main.tf.  
+//             Depending on SDLC environmental setting, different attributes are passed to create the stack 
+//
+// Developer: 	Bobby Wen, bobby@wen.org
+//======================================================================================
 provider "azurerm" {
   features {}
-  ### NOTE: default_tags not available for azurerm provider
+  # NOTE: default_tags not available for azurerm provider
 }
 
-###  Setting up a Kubernetes Cluster with Monitoring Enabled
+#  Setting up a Kubernetes Cluster with Monitoring Enabled
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-k8s-resources"
   location = var.location[var.environment]
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "main" {
   service_endpoints    = ["Microsoft.Sql"]
 }
 
-### Monitoring
+# Monitoring
 resource "azurerm_log_analytics_workspace" "main" {
   name                = "${var.prefix}-law"
   location            = azurerm_resource_group.main.location
